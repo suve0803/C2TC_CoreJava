bc66206c0900cac20858b36fb229793ad4dbe001
Administrator <Administrator@Suvetha>
2023-07-27 10:50:14 +0530

    first commit

---
+++ C2TC/src/com/cg/dayone/Basics.java
@@ -0,0 +1,20 @@
+package com.cg.dayone;
+
+public class Basics {
+
+	public static void main(String[] args) {
+		int a=10,b=30;
+		System.out.println("a+b="+(a+b));
+		System.out.println("a-b="+(a-b));
+		System.out.println("a*b="+(a*b));
+		System.out.println("a/b="+(a/b));
+		System.out.println("a%b="+(a%b));
+		System.out.println("a>b="+(a>b));
+		System.out.println("a<b="+(a<b));
+		System.out.println("a==b="+(a==b));
+		System.out.println("Increment="+(a++));
+		System.out.println("Decrement="+(a--));
+
+	}
+
+}
